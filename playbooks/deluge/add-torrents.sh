471129fc483ac571f91991fb9a4eaf2712f0a6a0
apollo <moraldisabled@gmail.com>
2018-03-18 10:57:30 +0300

    add playbooks

---
+++ playbooks/deluge/add-torrents.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+
+#$1 source directory with torrents file
+#$2 target path downloads file
+
+IFS=$'\n' 
+
+for iFile in $(find $1 -type f -name "*.torrent")
+do
+    add_torrent="add '$iFile' -p "$2" """
+
+    if deluge-console $add_torrent; then
+        rm $iFile 
+    fi
+done
